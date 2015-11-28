ff0107b1659701e0acdb9007a7b215c900140049
郭佳哲 <346672577@qq.com>
2015-11-28 10:06:04 +0800

    login

--- MobileAssignment/app/src/main/java/com/season/scut/LoginActivity.java
+++ MobileAssignment/app/src/main/java/com/season/scut/LoginActivity.java
@@ -6,10 +6,20 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.RequestParams;
+import com.season.scut.net.HttpClient;
+import com.season.scut.net.JsonResponseHandler;
+import com.season.scut.net.RequestParamName;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Administrator on 2015/11/27.
@@ -39,8 +49,8 @@ public class LoginActivity extends Activity {
         mBtnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!TextUtils.isEmpty(mEtpasswd.getText().toString())&&!TextUtils.isEmpty(mEtname.getText().toString())){
-                    netLogin(mEtname.getText().toString(),mEtpasswd.getText().toString());
+                if (!TextUtils.isEmpty(mEtpasswd.getText().toString()) && !TextUtils.isEmpty(mEtname.getText().toString())) {
+                    netLogin(mEtname.getText().toString(), mEtpasswd.getText().toString());
                 }
             }
         });
@@ -48,25 +58,49 @@ public class LoginActivity extends Activity {
         mTvregister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(LoginActivity.this,RegisterActivity.class);
-                startActivityForResult(intent,1);
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivityForResult(intent, 1);
             }
         });
     }
 
     public void netLogin(String name,String passwd){
-        //TODO
-        savaLoginState(12342342);
+        RequestParams params = new RequestParams();
+        params.put(RequestParamName.USERNAME, name);
+        params.put(RequestParamName.PASSWORD, passwd);
+        HttpClient.post(this, "user/login", params, new JsonResponseHandler() {
+            @Override
+            public void onSuccess(JSONObject response) {
+                try {
+                    savaLoginState(response.getJSONObject("data").getJSONObject("user").getString("token"));
+                    SharedPreferences preferences =getPreferences(MODE_PRIVATE);
+                    String token = preferences.getString(KEY_TOKEN, null);
+                    Log.i("token", token);
+                    startActivity(new Intent(getApplication(), MainActivity.class));
+                    finish();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(String message, String for_param) {
+                if (message != null) {
+                    Toast.makeText(getApplication(), message, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
     public boolean isLogined(){
         return getPreferences(MODE_PRIVATE).getInt(KEY_LOGINSTATE,VALUE_NOT_LOGIN)!=VALUE_NOT_LOGIN;
     }
 
-    public void savaLoginState(long token){
+    public void savaLoginState(String token){
         SharedPreferences preferences =getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor editor =preferences.edit();
-        editor.putLong(KEY_TOKEN,token);
+        editor.putString(KEY_TOKEN, token);
         editor.putInt(KEY_LOGINSTATE,VALUE_LOGINED);
         editor.commit();
     }
--- MobileAssignment/app/src/main/java/com/season/scut/net/RequestParamName.java
+++ MobileAssignment/app/src/main/java/com/season/scut/net/RequestParamName.java
@@ -5,4 +5,7 @@ package com.season.scut.net;
  * Created by gjz on 11/2/15.
  */
 public class RequestParamName {
+    public static final String TOKEN = "token";//用户token
+    public static final String USERNAME = "username";//登录时的用户名
+    public static final String PASSWORD = "password";//密码
 }
